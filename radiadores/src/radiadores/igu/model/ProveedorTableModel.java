e02f18f50b5253cae93e453b66d45e7dc29325e2
stafoxter <stafoxter@74213a12-9ed2-11dd-a3f3-7b6103e1383c>
2008-10-27 15:44:04 +0000

    metodos "crear" en los paneles Empleado y MP .. agregue los atributos : codigo y estado en la entidad Empleado y estado en MP

---
+++ radiadores/src/radiadores/igu/model/ProveedorTableModel.java
@@ -0,0 +1,148 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package radiadores.igu.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import radiadores.entidades.Proveedor;
+
+/**
+ *
+ * @author stafoxter
+ */
+
+public class ProveedorTableModel extends AbstractTableModel {
+    private static final long serialVersionUID = 1L;
+    private static final String[] NOMBRE_COLUMNAS = {"Nombre", "Telefono", "Contacto"};
+    private static final boolean[] COLUMNAS_EDITABLES = {false, false, false};
+    private static final Class[] CLASE_COLUMNAS =
+        {String.class, String.class, String.class};
+    
+    private List<Proveedor> proveedores;
+
+    /**
+     * Constructor
+     * 
+     * @param filas Cantidad de filas iniciales
+     */
+    public ProveedorTableModel(int filas) {
+        this.proveedores = new ArrayList<Proveedor>(filas > 0 ? filas : 0);
+    }
+
+    /**
+     * Devuelve el nombre de la columna
+     * 
+     * @param columna Indice de la columna
+     * @return El nombre de la columna
+     */
+    @Override
+    public String getColumnName(int columna) {
+        return NOMBRE_COLUMNAS[columna];
+    }
+    
+    /**
+     * Devuelve la clase de la columna indicada
+     * 
+     * @param columna Indice de la columna de la que se quiere averiguar la clase
+     * @return La clase de la columna indicada
+     */
+    @Override
+    public Class<?> getColumnClass(int columna) {
+        return CLASE_COLUMNAS[columna];
+    }
+
+    /**
+     * Devuelve si la celda es o no editable
+     * 
+     * @param fila Indice de la fila
+     * @param columna Indice de la columna
+     * @return True si la celda es editable
+     */
+    @Override
+    public boolean isCellEditable(int fila, int columna) {
+        return COLUMNAS_EDITABLES[columna];
+    }
+    
+    /**
+     * Devuelve la cantidad de filas
+     * 
+     * @return La cantidad de filas
+     */
+    @Override
+    public int getRowCount() {
+        return proveedores.size();
+    }
+
+    /**
+     * Devuelve la cantidad de columnas
+     * 
+     * @return La cantidad de columnas
+     */
+    @Override
+    public int getColumnCount() {
+        return NOMBRE_COLUMNAS.length;
+    }
+
+    /**
+     * Devuelve el valor de la celda indicada
+     * 
+     * @param fila Indice de la fila
+     * @param columna Indice de la columna
+     * @return Devuelve el valor de la celda indicada
+     */
+    @Override
+    public Object getValueAt(int fila, int columna) {
+        Object resultado = null;
+        
+        switch(columna) {
+            case 0:
+                resultado = proveedores.get(fila).getNombreProveedor();
+                break;
+            case 1:
+                resultado = proveedores.get(fila).getTelefono();
+                break;
+            case 2:
+                resultado = proveedores.get(fila).getNombreContacto();
+                break;
+        }
+        return resultado;
+    }
+    
+    /**
+     * Agrega Proveedor al modelo
+     * 
+     * @param proveedor Proveedor a agregar
+
+     */
+    public void agregarFila(Proveedor proveedor) {
+        proveedores.add(proveedor);
+        
+        fireTableRowsInserted(proveedores.size(), proveedores.size());
+    }
+    
+    /**
+     * Limita la cantidad de elementos del modelo al indicado
+     * 
+     * @param cantidad Cantidad a la que se quiere limitar el numero de filas
+     */
+    public void limitarCantidad(int cantidad) {
+        int cantidadAnterior = proveedores.size();
+        
+        proveedores = proveedores.subList(0, cantidad);
+        
+        fireTableRowsDeleted(cantidad, cantidadAnterior);
+    }
+    
+    /**
+     * Devuelve todas las filas del modelo
+     * 
+     * @return Todas las filas del modelo
+     */
+    public List<Proveedor> getFilas() {
+        return proveedores;
+    }
+}
